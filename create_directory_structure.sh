0a1ea6efff4d570c2476eb811ef126984b87b298
ciiram <cwamaina.dekut@gmail.com>
2018-01-18 16:20:17 +0300

    file to create directory structure

---
+++ create_directory_structure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This script creates the directories necessary
+#
+
+
+
+
+#make directories
+mkdir data
+mkdir files
+mkdir figures
+mkdir data/dekuwc
+mkdir data/xeno_canto
+mkdir data/xeno_canto/mp3
+mkdir data/xeno_canto/wav-16kHz
+mkdir data/dekuwc/wav-16kHz
+mkdir figures/HATU_Spectrograms/
+
